0e0f6dc9ebce15c94abced8f8dfa26d897fd7131
Manu <noronamanu@gmail.com>
2026-09-14 14:11:33 -0300

    Agregados los include de las clases al main

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "clases/adoptante.cpp"
+#include "clases/animal.cpp"
+#include "clases/refugio.cpp"
+#include "clases/solicitudAdopcion.cpp"
+#include "clases/perro.cpp"
+#include "clases/gato.cpp"
+#include <iostream>
