a65590104edce6bc940e2ab9817b2c36ebd33e51
VicenG9054 <2609054@ucc.edu.ar>
2026-09-21 12:53:29 -0300

    Constructor header/Clase gato

--- headers/gato.h
+++ headers/gato.h
@@ -10,4 +10,7 @@ class Gato : protected Animal {
     private:
     std::string pelaje;
     Condicion condicion;
+
+Gato(int ide, std::string nom, int age, bool adopt, EstadoDeSalud est, std::string pelaj, Condicion cond); 
+    
 };
