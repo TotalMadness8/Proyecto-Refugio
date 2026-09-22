0652c97fbd8ba9dea785efabea4ad430a4f3ac32
VicenG9054 <2609054@ucc.edu.ar>
2026-09-22 13:41:02 -0300

    Implementacion del metodo en Clase Perro

--- clases/perro.cpp
+++ clases/perro.cpp
@@ -1,6 +1,13 @@
 #include "headers/perro.h"
 
 Perro::Perro(int id, std::string nom, int age, bool adopt, EstadoDeSalud est, std::string raz, Tamanio tam)
-    : Animal(id, nom, age, adopt, est), raza(raz), size(tam){
+    : Animal(id, nom, age, adopt, est), raza(raz), size(tam) { }
 
-    }
+void Perro::mostrarInfo() const {
+    std::cout << "[Perro] ";
+    
+    Animal::mostrarInfo();                  //Muestra la info general del animal
+    
+    std::cout << " | Raza: " << raza
+              << " | " << (size == Tamanio::Chico ? "Chico" : "Grande") << std::endl;        //Muestra la info especifica del perro
+}
